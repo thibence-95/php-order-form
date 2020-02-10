96d8f301cd1c5148f7c660680e566bf068acfebf
Thibence <salencethibaut95@gmail.com>
2020-02-10 16:48:12 +0100

    thhis is wrong on so many levels!

--- form-view.php
+++ form-view.php
@@ -5,10 +5,11 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
-          rel="stylesheet"/>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
     <title>Order food & drinks</title>
 </head>
+
+
 <body>
 <div class="container">
     <h1>Order food in restaurant "the Personal Ham Processors"</h1>
--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
-//this line makes PHP behave in a more strict way
-declare(strict_types=1);
 
-//we are going to use session variables so we need to enable sessions
-session_start();
+declare(strict_types = 1);
+
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+
+session_start(); //we are going to use session variables so we need to enable sessions
 
 function whatIsHappening() {
     echo '<h2>$_GET</h2>';
@@ -18,20 +22,92 @@ function whatIsHappening() {
 
 //your products with their price.
 $products = [
+    ['name' => 'Pesto Mozzarella', 'price' => 3],
     ['name' => 'Club Ham', 'price' => 3.20],
     ['name' => 'Club Cheese', 'price' => 3],
-    ['name' => 'Club Cheese & Ham', 'price' => 4],
+    ['name' => 'Club Ham & Cheese', 'price' => 4],
     ['name' => 'Club Chicken', 'price' => 4],
     ['name' => 'Club Salmon', 'price' => 5]
 ];
 
 $products = [
-    ['name' => 'Cola', 'price' => 2],
+    ['name' => 'Coke', 'price' => 2],
     ['name' => 'Fanta', 'price' => 2],
-    ['name' => 'Sprite', 'price' => 2],
-    ['name' => 'Ice-tea', 'price' => 3],
+    ['name' => '7up', 'price' => 2],
+    ['name' => 'Ice-tea', 'price' => 2],
+    ['name' => 'Ice-tea Green', 'price' => 3]
 ];
 
+$emailErr = "";
+$street = "";
+$streetErr = "";
+$streetnum = "";
+$streetnumErr = "";
+$city = "";
+$cityErr = "";
+$zip = "";
+$zipErr = "";
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    if (empty($_POST["email"])) {
+        $emailErr = "E-mail is required.";
+    }
+    else {
+        $email = ($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            $emailErr = "Invalid email format";
+        }
+    }
+
+
+    if (empty($_POST["street"])) {
+        $streetErr = "Street name is required";
+    }
+    else {
+        $street ($_POST["street"]);
+        if (!is_string($_POST["street"])) {
+            $streetErr = "Invalid street name";
+        }
+    }
+
+    if (empty ($_POST["streetnumber"])) {
+        $streetnumErr = "Street number is required";
+    }
+    else {
+        $streetnum ($_POST["streetnumber"]);
+        if (!is_numeric($_POST["streetnumber"])) {
+            $streetnumErr = "Invalid street number";
+        }
+    }
+
+    if (empty ($_POST["city"])) {
+        $cityErr = "City is required";
+    }
+    else {
+        $city ($_POST["city"]);
+        if (!is_string($_POST["city"])) {
+            $cityErr = "Invalid city";
+        }
+    }
+
+    if (empty ($_POST["zipcode"])) {
+        $zipErr = "Zip code is required";
+    }
+    else {
+        $zip ($_POST["zipcode"]);
+        if (!is_numeric($_POST["zipcode"])) {
+            $zipErr = "Invalid zip code";
+        }
+    }
+}
+
+echo $emailErr;
+
+
 $totalValue = 0;
+
+//whatIsHappening();
 
 require 'form-view.php';
