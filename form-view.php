b1d22f9d5ce3a4a636799c263c03fdd18cfa5779
Thibence <salencethibaut95@gmail.com>
2020-02-11 14:59:59 +0100

    got the data retention

--- form-view.php
+++ form-view.php
@@ -27,7 +27,7 @@
         <div class="form-row">
             <div class="form-group col-md-6">
                 <label for="email">E-mail:</label>
-                <input type="text" id="email" name="email" class="form-control"/>
+                <input type="text" id="email" name="email" class="form-control" value="<?php echo isset($_POST['email']) ? $_POST['email'] : '' ?>"/>
                 <?php echo $emailErr; ?>
             </div>
             <div></div>
@@ -39,24 +39,24 @@
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control">
+                    <input type="text" name="street" id="street" class="form-control"value="<?php echo isset($_POST['street']) ? $_POST['street'] : '' ?>">
 	                <?php echo $streetErr; ?>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="streetnumber">Street number:</label>
-                    <input type="number" id="streetnumber" name="streetnumber" class="form-control">
+                    <input type="number" id="streetnumber" name="streetnumber" class="form-control"value="<?php echo isset($_POST['streetnumber']) ? $_POST['streetnumber'] : '' ?>">
                     <?php echo $streetnumErr; ?>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control">
+                    <input type="text" id="city" name="city" class="form-control"value="<?php echo isset($_POST['city']) ? $_POST['city'] : '' ?>">
                     <?php echo $cityErr; ?>
                 </div>
                 <div class="form-group col-md-6">
                     <label for="zipcode">Zipcode</label>
-                    <input type="number" id="zipcode" name="zipcode" class="form-control">
+                    <input type="number" id="zipcode" name="zipcode" class="form-control"value="<?php echo isset($_POST['zipcode']) ? $_POST['zipcode'] : '' ?>">
                     <?php echo $zipErr; ?>
                 </div>
             </div>
